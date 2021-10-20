9185aa1678c672dbcb823fdd6faf436c9810b8b0
Orangzaib Khan <Orangzaib Khan@DESKTOP-A94KPUN>
2021-10-20 10:30:26 +0500

    Updated

---
+++ Seraat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Seraat.MvcApplication" Language="C#" %>
---
+++ Seraat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Seraat.Startup))]
+namespace Seraat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
